6d3a832fe631fb2ce3ab65ad747c70a36d0295a6
Bosellicious <abosedeosoba@gmail.com>
2022-09-13 11:20:49 +0100

    Complete challenge 5

--- algorithms/challenge-5.js
+++ algorithms/challenge-5.js
@@ -1,5 +1,8 @@
 function filterList(arr) {
- 
- }
+    let newList = arr.filter((char) => typeof char == "number")
+    return newList;
+}
 
-  module.exports =filterList
+filterList([1,2,'aasf','1','123',123]);
+
+module.exports =filterList

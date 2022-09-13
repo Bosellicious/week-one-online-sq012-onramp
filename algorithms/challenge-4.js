061f429fe436ecfa06411330e701b702b3265b4c
Bosellicious <abosedeosoba@gmail.com>
2022-09-13 11:14:45 +0100

    Complete challenge 4

--- algorithms/challenge-4.js
+++ algorithms/challenge-4.js
@@ -1,5 +1,12 @@
 function sumTwoSmallestNums(arr) {
-    
- 
- }
-  module.exports =sumTwoSmallestNums
+    let filtered = arr.filter((x)=> Math.sign(x) == 1)
+    let sorted = filtered.sort((a,b) => a - b);
+
+    let num1 = sorted[0]
+    let num2 = sorted[1]
+    return num1 + num2;
+}
+
+sumTwoSmallestNums([2, 9, 6, -1]);
+
+module.exports =sumTwoSmallestNums

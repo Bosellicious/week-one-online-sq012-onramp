3fb02fd818883eb3410ab7bd287f11d70576f7a4
Bosellicious <abosedeosoba@gmail.com>
2022-09-13 10:16:41 +0100

    Complete challenge 3

--- algorithms/challenge-3.js
+++ algorithms/challenge-3.js
@@ -1,5 +1,7 @@
 function sumMix(arr) {
-   
- }
+   return arr.reduce((total, int) => Number(total) + Number(int))
+}
 
-  module.exports = sumMix
+sumMix(['5', '0', 9, 3, 2, 1, '9', 6, 7]);
+
+module.exports = sumMix
